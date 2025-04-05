ff7c62b237a3dd6b476dcce855e27efef0988473
Ilyas Khisyametdinov <hopi@mail.ru>
2025-04-05 17:55:25 +0400

    trying to deploy

--- src/components/CatList/CatListItem/CatListItem.tsx
+++ src/components/CatList/CatListItem/CatListItem.tsx
@@ -6,34 +6,37 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../store/store";
 import { catsActions } from "../../../store/cats.slice";
 import RemoveIcon from "../../../assets/Remove-icon.svg?react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function CatListItem({ cat }: { cat: ICat }) {
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
 
-  const likeHandler = () => {
+  const likeHandler = (event: React.MouseEvent) => {
+    event.stopPropagation();
     dispatch(catsActions.like(cat.id));
   };
 
-  const removeHandler = () => {
+  const removeHandler = (event: React.MouseEvent) => {
+    event.stopPropagation();
     dispatch(catsActions.delete(cat.id));
   };
+
+  const handlerCardClick = () => {
+    navigate(`/cats/${cat.id}`);
+  };
   return (
-    <Link to={`/cats/${cat.id}`}>
-      <div className={styles.card}>
-        <img className={styles.img} src={cat.url} alt={cat.breeds[0].name} />
-        <h3 className={styles.name}>{cat.breeds[0].name}</h3>
-        <div className={styles.description}>{cat.breeds[0].description}</div>
-        <div className={styles.like} onClick={likeHandler}>
-          <Heart
-            className={cn(styles.heart, { [styles.liked]: cat.isLiked })}
-          />
-        </div>
-        <div className={styles.remove} onClick={removeHandler}>
-          <RemoveIcon />
-        </div>
+    <div className={styles.card} onClick={handlerCardClick}>
+      <img className={styles.img} src={cat.url} alt={cat.breeds[0].name} />
+      <h3 className={styles.name}>{cat.breeds[0].name}</h3>
+      <div className={styles.description}>{cat.breeds[0].description}</div>
+      <div className={styles.like} onClick={likeHandler}>
+        <Heart className={cn(styles.heart, { [styles.liked]: cat.isLiked })} />
       </div>
-    </Link>
+      <div className={styles.remove} onClick={removeHandler}>
+        <RemoveIcon />
+      </div>
+    </div>
   );
 }
 
